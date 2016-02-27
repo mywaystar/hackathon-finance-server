4359cf62e9d6e320977c8b681bc129c35b59d3c2
peltoche <dev@halium.fr>
2016-02-27 09:29:29 +0100

    Add base server

--- server/app.js
+++ server/app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var mysql = require("./routes/db")
 
 var app = express();
 
@@ -21,6 +22,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(mysql.init_config());
 
 app.use('/', routes);
 app.use('/users', users);
